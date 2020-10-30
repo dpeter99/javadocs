4db95a22558e8beb79e14d0beb0d0e287a2500eb
dpeter99 <dpeter99@users.noreply.github.com>
2020-10-30 09:28:10 +0000

    deploy: 2c5137322167dcb4719c498a678fed7b34abfe41

--- annotated_dup.js
+++ annotated_dup.js
@@ -1,13 +1,5 @@
 var annotated_dup =
 [
-    [ "net", null, [
-      [ "minecraft", null, [
-        [ "advancements", null, [
-          [ "Advancement", "classnet_1_1minecraft_1_1advancements_1_1_advancement.html", null ]
-        ] ],
-        [ "util", null, [
-          [ "FileUtil", "classnet_1_1minecraft_1_1util_1_1_file_util.html", null ]
-        ] ]
-      ] ]
-    ] ]
+    [ "Advancement", "classnet_1_1minecraft_1_1advancements_1_1_advancement.html", null ],
+    [ "FileUtil", "classnet_1_1minecraft_1_1util_1_1_file_util.html", null ]
 ];
--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['advancement_0',['Advancement',['../classnet_1_1minecraft_1_1advancements_1_1_advancement.html',1,'net::minecraft::advancements']]]
+  ['advancement_0',['Advancement',['../classnet_1_1minecraft_1_1advancements_1_1_advancement.html',1,'']]]
 ];
