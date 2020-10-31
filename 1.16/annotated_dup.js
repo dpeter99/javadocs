938a2a48f154132f0c89342af36590fb974df396
dpeter99 <dpeter99@users.noreply.github.com>
2020-10-31 23:13:37 +0000

    deploy: bc0e2647b67e4fce575dbe261e00225583d7a419

--- 1.16/annotated_dup.js
+++ 1.16/annotated_dup.js
@@ -1,5 +1,13 @@
 var annotated_dup =
 [
-    [ "Advancement", "classnet_1_1minecraft_1_1advancements_1_1_advancement.html", null ],
-    [ "FileUtil", "classnet_1_1minecraft_1_1util_1_1_file_util.html", null ]
+    [ "net", null, [
+      [ "minecraft", null, [
+        [ "advancements", null, [
+          [ "Advancement", "classnet_1_1minecraft_1_1advancements_1_1_advancement.html", null ]
+        ] ],
+        [ "util", null, [
+          [ "FileUtil", "classnet_1_1minecraft_1_1util_1_1_file_util.html", null ]
+        ] ]
+      ] ]
+    ] ]
 ];
